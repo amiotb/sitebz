9579ecd86fb6ca343dd755446dbeb648b7a4827c
amiotb <34064339+amiotb@users.noreply.github.com>
2023-12-24 17:03:42 +0100

    Create hello.js

---
+++ src/posts/hello.js
@@ -0,0 +1,24 @@
+export default {
+  id: "hello",
+  title: "The hello world",
+  content: `
+    <p>
+        Lorem ipsum dolor sit amen
+    </p>
+`,
+
+  images: [
+    {
+      url: "/images/1.png",
+      text: "Just a bird",
+    },
+    {
+      url: "/images/bird.mp4",
+      text: "Just a second bird",
+    },
+    {
+      url: "/images/1.png",
+      text: "Just a third bird",
+    },
+  ],
+};
