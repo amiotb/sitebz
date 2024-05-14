094db8afbfada7cea092259f748fc588f74d5a39
amiotb <34064339+amiotb@users.noreply.github.com>
2024-05-14 22:30:25 +0200

    Update bird.js

--- src/posts/bird.js
+++ src/posts/bird.js
@@ -1,24 +1,34 @@
 export default {
-  id: "bird",
-  title: "The Bird",
+  id: "chapitre9",
+  title: "Résidence de territoire",
   content: `
     <p>
-        Lorem ipsum dolor sit amen
+Chapitre 9 est une résidence de territoire à Liffré-Cormier Communauté d'une année. 
+Associée avec l'association Electroni[k], Bérengère Amiot accompagne le réseau de neuf bibliothèques autour d’un projet collaboratif : créer un livre alliant papier et numérique. 
+Chapitre 9 offre une approche ludique et décomplexée sur la diversité de la création numérique et sa portée inclusive.
+La résidence s’est structurée en 3 étapes : 
+- se former aux formes artistiques numérique
+– créer avec 27 ateliers et plus de 200 participant.e.s
+- valoriser un projet collaboratif avec un livre interactif présenté sur la 20ème édition de Maintenant festival.
     </p>
 `,
 
   images: [
     {
-      url: "/images/1.png",
-      text: "Just a bird",
+      url: "/images/chapitre91.jpg",
+      text: "Dessin Numérique",
     },
     {
-      url: "/images/bird.mp4",
-      text: "Just a second bird",
+      url: "/images/chapitre92.jpg",
+      text: "Sensibilisation à la création numérique",
     },
     {
-      url: "/images/1.png",
-      text: "Just a third bird",
+      url: "/images/chapitre91.jpg",
+      text: "Festival Maintenant édition 2021, Rennes",
+    },
+       {
+      url: "/images/Chapitre 9 v.mp4",
+      text: "Vidéo Vincent Cadoret",
     },
   ],
 };
