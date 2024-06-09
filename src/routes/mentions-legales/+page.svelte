66197a414b1604844742987e64c09bb3f17c4187
amiotb <34064339+amiotb@users.noreply.github.com>
2024-06-09 22:30:37 +0200

    Update +page.svelte

--- src/routes/mentions-legales/+page.svelte
+++ src/routes/mentions-legales/+page.svelte
@@ -6,18 +6,24 @@
 
 <div class="prose sm:prose-lg md:prose-xl mt-10 prose-h1:text-center">
   <h2>Mentions Légales</h2>
-  <font face="OstiaAnticaBookItalic">Identité</font>
+  <p>
+<font face="OstiaAnticaBookItalic">Identité</font>
+</p>
   <p>
     Nom du site : berengereamiot.com
     Adresse du site : https://www.berengereamiot.com
     Propriétaire : Bérengère Amiot 35000 Rennes
     SIRET 751 711 201 00044
   </p>
-  <font face="OstiaAnticaBookItalic">Hébergeur du site</font>
+ <p>  
+<font face="OstiaAnticaBookItalic">Hébergeur du site</font> 
+</p>
   <p>
   Hébergement du site : OVH - 2 rue Kellermann - 59100 Roubaix - France
   </p>
+ <p>
   <font face="OstiaAnticaBookItalic">Propriété intellectuelle</font>
+  </p>
   <p>
     Le contenu de ce site web est protégé par les lois sur la propriété
     intellectuelle. Tous les droits de reproduction sont réservés, y compris les
