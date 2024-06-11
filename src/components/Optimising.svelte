63b1dd27516d48bd50fa3739b97de3e9eb4a1fa1
amiotb <34064339+amiotb@users.noreply.github.com>
2024-06-11 12:09:14 +0200

    Update Optimising.svelte

--- src/components/Optimising.svelte
+++ src/components/Optimising.svelte
@@ -1,4 +1,4 @@
 <svelte:head>
-    <link rel="preload" href="/fonts/GT-Sectra-Fine-Book.ttf" as="font" type="font/truetype">
-    <link rel="preload" href="/fonts/GT-Sectra-Fine-Medium.ttf" as="font" type="font/truetype">
+    <link rel="preload" href="/fonts/OstiaAntica-Regular.otf" as="font" type="font/truetype">
+    <link rel="preload" href="/fonts/OstiaAntica-BookItalic.otf" as="font" type="font/truetype">
 </svelte:head>
