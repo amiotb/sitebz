5cce15a996709ac19f02cdf40ec4f6957cf3d4b7
amiotb <34064339+amiotb@users.noreply.github.com>
2024-06-14 16:55:09 +0200

    Update chapitre9.js

--- src/posts/chapitre9.js
+++ src/posts/chapitre9.js
@@ -8,14 +8,14 @@ export default {
 Associée avec l'association Electroni[k], Bérengère Amiot accompagne le réseau de neuf bibliothèques autour d’un projet collaboratif : créer un livre alliant papier et numérique. 
  <span style="font-family:OstiaAnticaBookItalic;">Chapitre 9</span> offre une approche sur la diversité de la création numérique et sa portée inclusive.
 <br>
-La résidence d'1 an s’est structurée en 3 étapes : </br>
-<br>
-- se former aux formes artistiques numérique </br>
-<br>
-– créer avec 27 ateliers et plus de 200 participant.e.s </br>
-<br>
+La résidence d'1 an s’est structurée en 3 étapes :
+
+- se former aux formes artistiques numérique
+
+– créer avec 27 ateliers et plus de 200 participant.e.s
+
 - valoriser un projet collaboratif avec un livre interactif présenté sur la 20ème édition de Maintenant festival.</br>
-    </p>
+  </br>  </p>
 `,
 
   images: [
