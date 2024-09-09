2743dc71913093802c80bdeda97d3af0de163069
amiotb <34064339+amiotb@users.noreply.github.com>
2024-09-09 19:00:53 +0200

    Update urbain.js

--- src/posts/urbain.js
+++ src/posts/urbain.js
@@ -3,23 +3,24 @@ export default {
   title: "Workshop Design Urbain",
   content: `
     <p>
-Bérengère Amiot invite 40 élèves de maternelle à voir, écouter et ressentir la ville autrement pendant 1 semaine.
-À travers des ateliers d'observation et d'expérimentation, les enfants appréhendent leur quartier pour mieux comprendre ce qu'est l'espace urbain.
+Bérengère Amiot invite 40 élèves de maternelle à voir, écouter et ressentir la ville autrement.
+À travers des ateliers d'observation et d'expérimentation, les enfants appréhendent leur quartier pour mieux comprendre ce qu'est l'espace public.
+Car une ville n'est pas qu'un ensemble de bâtiments et de rues, elle est également modelée par les usages qu'en font les habitants à travers le mobilier urbain.    
 Pont-Audemer juin 2021
 </p>`,
 
   images: [
     {
       url: "/images/urbain1.jpeg",
-      text: "Comprendre le lien entre la forme et la fonction",
+      text: "Comprendre le lien entre la forme et la fonction dans le design",
     },
     {
       url: "/images/urbain2.jpeg",
-      text: "Observer l'espace urbain",
+      text: "Observer l'espace urbain pour mieux l'appréhender",
     },
     {
       url: "/images/urbain3.jpeg",
-      text: "Dessiner sa maison de mémoire",
+      text: "Décrire sa maison par les mots et l'image",
     },
      {
       url: "/images/urbain4.jpeg",
@@ -27,11 +28,11 @@ Pont-Audemer juin 2021
     },
       {
       url: "/images/urbain5.jpeg",
-      text: "Comprendre la carte de son quartier",
+      text: "Explorer les signes topographiques d'une carte",
     },
       {
       url: "/images/urbain6.jpeg",
-      text: "Imaginer un espace commun",
+      text: "Imaginer une place publique",
     },
   ],
 };
