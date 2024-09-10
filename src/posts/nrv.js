0cb890193c0223e126785562f3399aac313bf4d2
amiotb <34064339+amiotb@users.noreply.github.com>
2024-09-10 17:56:46 +0200

    Update and rename lamp.js to nrv.js

---
+++ src/posts/nrv.js
@@ -0,0 +1,36 @@
+export default {
+  id: "nrv",
+  title: "Nos Réalités Virtuelles",
+    position: 1,
+  content: `
+    <p>
+Les imaginaires géographiques se construisent en fonction de notre culture, notre histoire, notre littérature et nos médias. 
+Ils jouent un rôle prépondérant dans la manière dont nous comprenons et interagissons avec le monde, 
+façonnant ainsi notre perception au gré des récits culturels, des stéréotypes, des représentations artistiques et des expériences individuelles.
+Les intelligences artificielles, en perpétuant des paysages idéalisés (comme des prairies verdoyantes, des montagnes majestueuses, des rivières paisibles...) 
+reflètent et amplifient ces stéréotypes préexistants.
+<br>
+L’avènement des technologies de navigation, particulièrement du GPS, engendre une distorsion de nos imaginaires géographiques. 
+La dépendance excessive à la navigation assistée par GPS induit une altération significative des compétences d'orientation spatiale et de la perception de l'environnement. 
+En privilégiant les itinéraires dictés par ces applications, nos imaginaires géographiques risquent de subir une distorsion, sapant ainsi notre lien authentique avec la réalité.
+<span style="font-family:OstiaAnticaBookItalic;">Nos Réalités Virtuelles</span> dessine avec la participation des visiteurs, une carte imaginaire d'un paysage à partir de rien. 
+Un exercie pour enrichir notre capacité à concevoir, encore et toujours, des environnements fictifs.
+<br>
+Le projet a bénéficié d'une bourse d'aide à la création de la ville de Rennes, 2023.</p>
+`,
+
+  images: [
+    {
+      url: "/images/nrv 1.png",
+      text: "Bivouac, l'IA nous raconte le monde",
+    },
+    {
+      url: "/images/Nos Réalités Virtuelles_bd.mp4",
+      text: "vidéo aux Ateliers du Vent",
+    },
+    {
+      url: "/images/nrv 2.png",
+      text: "Terraformer, imaginer un relevé topographique",
+    },
+  ],
+};
