f365e2350eba2b707e9d6b9609b02a8b7bd05449
amiotb <34064339+amiotb@users.noreply.github.com>
2025-02-20 22:16:51 +0100

    Update nrv.js

--- src/posts/nrv.js
+++ src/posts/nrv.js
@@ -4,19 +4,23 @@ export default {
     position: 1,
   content: `
     <p>
-Les imaginaires géographiques se construisent en fonction de notre culture, notre histoire, notre littérature et nos médias. 
-Ils jouent un rôle prépondérant dans la manière dont nous comprenons et interagissons avec le monde, 
-façonnant ainsi notre perception au gré des récits culturels, des stéréotypes, des représentations artistiques et des expériences individuelles.
-Les intelligences artificielles, en perpétuant des paysages idéalisés (comme des prairies verdoyantes, des montagnes majestueuses, des rivières paisibles...) 
-reflètent et amplifient ces stéréotypes préexistants.
+Les imaginaires géographiques, amalgames de représentations mentales et culturelles de l’espace, subissent une profonde influence de divers facteurs tels que la culture, 
+l’histoire, la littérature et les médias. Ils jouent un rôle prépondérant dans la manière dont les individus comprennent et interagissent avec le monde qui les entoure, 
+façonnant ainsi leur perception au gré des récits culturels, des stéréotypes, des représentations artistiques et des expériences individuelles.
 <br>
-L’avènement des technologies de navigation, particulièrement du GPS, engendre une distorsion de nos imaginaires géographiques. 
-En privilégiant les itinéraires dictés par ces applications, nos imaginaires géographiques risquent de subir une distorsion, sapant ainsi notre lien authentique avec la réalité.
+Les intelligences artificielles, en perpétuant des paysages idéalisés (comme des prairies verdoyantes, des montagnes majestueuses, des rivières paisibles...) reflètent et amplifient ces stéréotypes préexistants.
+L’avènement des technologies de navigation, particulièrement du GPS, engendre une distorsion de nos imaginaires géographiques. La dépendance excessive à la navigation assistée par GPS, 
+communément désignée comme le «mal du GPS», induit une altération significative des compétences d’orientation spatiale et de la perception de l’environnement. 
 <br>
-<span style="font-family:OstiaAnticaBookItalic;">Nos Réalités Virtuelles</span> dessine avec la participation des visiteurs, une carte imaginaire d'un paysage à partir de rien. 
-Un exercie pour enrichir notre capacité à concevoir, encore et toujours, des environnements fictifs.
+Cette préférence pour des itinéraires préétablis entraîne une réduction des compétences d’orientation spatiale, engendrant une désaffection envers les éléments naturels. 
+En privilégiant les itinéraires dictés par ces applications, nos imaginaires géographiques subissent une distorsion, sapant ainsi notre lien authentique avec la réalité.
+<span style="font-family:OstiaAnticaBookItalic;">Nos Réalités Virtuelles</span> propose aux spectateurs de créer une cartographie imaginaire d’un paysage issu du néant, 
+un exercice qui peut potentiellement enrichir notre capacité à concevoir encore et toujours des environnements fictifs.
 <br>
-Le projet a bénéficié d'une bourse d'aide à la création de la ville de Rennes, 2023.</p>
+<span style="font-family:OstiaAnticaBookItalic;">Le projet a bénéficié d'une bourse d'aide à la création de la ville de Rennes, 2023.</span>
+<br>
+<span style="font-family:OstiaAnticaBookItalic;">Installation proposée pendant la Buvette des Ateliers du Vent, janvier 2024</span>
+</p>
 `,
 
   images: [
@@ -26,7 +30,7 @@ Le projet a bénéficié d'une bourse d'aide à la création de la ville de Renn
     },
     {
       url: "/images/Nos Réalités Virtuelles_bd.mp4",
-      text: "vidéo aux Ateliers du Vent",
+      text: "vidéo Buvette aux Ateliers du Vent 2024",
     },
     {
       url: "/images/nrv 2.png",
