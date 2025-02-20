2945659257d7e47820ba8c6db5c242ba3f79cd83
amiotb <34064339+amiotb@users.noreply.github.com>
2025-02-21 00:33:53 +0100

    Update labodesign.js

--- src/posts/labodesign.js
+++ src/posts/labodesign.js
@@ -1,25 +1,37 @@
 export default {
-  id: "labodesign",
-  title: "Le Laboratoire de design",
-  position: 5,
+  id: "popup",
+  title: "Pop Up",
+  position: 8,
   content: `
     <p>
-        Lorem ipsum dolor sit amen
-    </p>
+Pop-Up est une installation conçue par neuf femmes détenues au Centre Pénitentiaire de Rennes. 
+Durant un mois, elles ont écrit, dessiné et conceptualisé une maquette de la prison, livrant un regard à la fois riche et nuancé sur leur quotidien, en racontant notamment ce qu’elles voient à travers leurs fenêtres.
+Situé en plein centre-ville de Rennes, le centre pénitentiaire est un paradoxe : les femmes peuvent entendre et parfois apercevoir la vie urbaine, sans pour autant la connaître véritablement.
+Elles passent du temps à arpenter du regard la vie citadine pour celles qui ont la chance d’avoir une fenêtre qui ne donne pas sur la cour.
+L’installation se compose de modules que les visiteurs peuvent manipuler, révélant des textes écrits par les femmes, qui décrivent leur vision sensible et personnelle de la prison. 
+Cette œuvre collective offre une perspective intime sur leur rapport à l’espace carcéral, tout en évoquant ce lien distant et mystérieux avec la ville environnante. 
+À travers ce projet, les participantes ont pu exprimer une voix souvent oubliée, transformant leur vécu en un objet artistique et participatif. 
+<br>
+    <span style="font-family:OstiaAnticaBookItalic;">Exposition Rennes Ma Ville, musée de Bretagne, 2018, Rennes et l'association Electroni[k]</span> 
+</p>
 `,
 
   images: [
     {
-      url: "/images/5.png",
-      text: "My table",
+      url: "/images/20180906_094157_138.jpg",
+      text: "Pop Up, objet cartographique",
     },
     {
-      url: "/images/2.png",
-      text: "Just a second bird",
+      url: "/images/20181124_182215_005.jpg",
+      text: "Pop Up, musée de Bretagne 2017",
     },
     {
-      url: "/images/1.png",
-      text: "Just a third bird",
+      url: "/images/20180906_093949_299.jpg",
+      text: "Pop Up, détail des barreaux",
+    },
+      {
+      url: "/images/book 2024 carto18.jpg",
+      text: "Pop Up, schéma",
     },
   ],
 };
